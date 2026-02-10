38a6b7b9eb041231b21db7fec351a416bc573fec
plangathamman-ops <plangathamman@gmail.com>
2026-02-10 03:55:31 +0300

    Update MongoDB Atlas connection

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
-dotenv.config();
+const path = require('path');
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 module.exports = {
   port: process.env.PORT || 5000,
